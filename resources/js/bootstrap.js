dac31eee6605d20b5369d92693c4e92280b58ab5
Jose Garcia <jogarcia@tuta.io>
2021-09-11 12:42:35 -0400

    Require popper before boostrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,6 +8,7 @@ window.axios = require("axios");
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
+require("@popperjs/core");
 require("bootstrap");
 require("leaflet");
 
