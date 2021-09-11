26a3ec047865470a13e0c0fc0a66eaf6310c4d13
Jose Garcia <jogarcia@tuta.io>
2021-09-11 17:00:09 -0400

    Create basic InfoDisplayer file

---
+++ resources/js/components/InfoDisplayer.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <leaflet-map></leaflet-map>
+        <div
+            class="btn-group position-fixed top-0 end-0 mt-3 ml-3"
+            role="group"
+            aria-label="Basic example"
+        >
+            <button
+                type="button"
+                class="btn btn-primary"
+                :class="{ active: active === 'map' }"
+            >
+                Mapa
+            </button>
+            <button
+                type="button"
+                class="btn btn-primary"
+                :class="{ active: active === 'table' }"
+            >
+                Tabla
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+import LeafletMap from "./LeafletMap.vue";
+
+export default {
+    data() {
+        return {
+            active: "map"
+        };
+    },
+    components: {
+        LeafletMap
+    },
+    name: "InfoDisplayer"
+};
+</script>
+
+<style scoped lang="scss">
+.btn-group {
+    z-index: 1000;
+    margin-right: 3em;
+}
+
+.active {
+    background-color: #1b4c75;
+    color: white;
+}
+</style>
