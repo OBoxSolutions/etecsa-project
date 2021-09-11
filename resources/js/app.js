d01cace017392e77bd3733551c40d105eb3ed838
Jose Garcia <jogarcia@tuta.io>
2021-09-11 16:22:59 -0400

    Add Vue support

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,32 @@
 require("./bootstrap");
 const leaflet = require("./leaflet");
+
+/**
+ * The following block of code imports vue components automatically
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+import Vue from "vue";
+window.Vue = Vue;
+
+const files = require.context("./", true, /\.vue$/i);
+files.keys().map(key =>
+    Vue.component(
+        key
+            .split("/")
+            .pop()
+            .split(".")[0],
+        files(key).default
+    )
+);
+
+/**
+ * Creates a vue instance
+ */
+
+const app = new Vue({
+    el: "#app"
+});
+
+module.exports = app;
