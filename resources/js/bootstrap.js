47bfba29eaae170bcaef6855b3c3cd767e881316
Jose Garcia <jogarcia@tuta.io>
2021-09-11 12:10:55 -0400

    Require leaflet

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,7 @@ window.axios = require("axios");
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 require("bootstrap");
+require("leaflet");
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
