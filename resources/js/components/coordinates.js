c4efede80831212ad9a7a1ba592e3609d032e77b
Jose Garcia <jogarcia@tuta.io>
2021-09-13 16:00:13 -0400

    Create coordinates file

---
+++ resources/js/components/coordinates.js
@@ -0,0 +1,572 @@
+export default [
+    {
+        Centro: "Caibarién",
+        Clave: "CE0",
+        Coordenadas: "22.5158, -79.4722"
+    },
+    {
+        Centro: "Cayo Santa María",
+        Clave: "KS0",
+        Coordenadas: "22.652071041861365, -78.9792973829921"
+    },
+    {
+        Centro: "Lagunas del Este",
+        Clave: "LGE",
+        Coordenadas: "22.662308521498524, -79.00071213893483"
+    },
+    {
+        Centro: "Dolores",
+        Clave: "DOL",
+        Coordenadas: "22.431210863611796, -79.42976838946254"
+    },
+    {
+        Centro: "Chiquitico",
+        Clave: "CHQ",
+        Coordenadas: "22.380436252552467, -79.54457749622684"
+    },
+    {
+        Centro: "San Agustín",
+        Clave: "SAG",
+        Coordenadas: "Ubícalos en el Cayo StaMaria"
+    },
+    {
+        Centro: "Salinas",
+        Clave: "SL0",
+        Coordenadas: "Ubícalos en el Cayo StaMaria"
+    },
+    {
+        Centro: "Hotel Dunas 6",
+        Clave: "DN6",
+        Coordenadas: "22.65805523688926, -79.05717792836234"
+    },
+    {
+        Centro: "Lagunas del Este 5",
+        Clave: "LG5",
+        Coordenadas: "22.663778600573856, -79.00054687726244"
+    },
+    {
+        Centro: "Van Troi",
+        Clave: "VT0",
+        Coordenadas: "22.5115867548559, -79.46963541398308"
+    },
+    {
+        Centro: "Cambaito",
+        Clave: "CB0",
+        Coordenadas: "22.501131001355773, -79.45672072142165"
+    },
+    {
+        Centro: "Marcelo Salado",
+        Clave: "MS0",
+        Coordenadas: "22.505673425136973, -79.49079926397859"
+    },
+    {
+        Centro: "Camajuani",
+        Clave: "CJ0",
+        Coordenadas: "22.46334438891047, -79.7213895005722"
+    },
+    {
+        Centro: "CAI Jose Maria Perez",
+        Clave: "JMP",
+        Coordenadas: "22.434252890057298, -79.68432351073571"
+    },
+    {
+        Centro: "Luis Arcos Bergnes",
+        Clave: "LB0",
+        Coordenadas: "22.505677194858478, -79.81683277864852"
+    },
+    {
+        Centro: "Batalla de Santa Clara",
+        Clave: "TL0",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Vueltas",
+        Clave: "VU0",
+        Coordenadas: "22.518426916176647, -79.7032684525141"
+    },
+    {
+        Centro: "GI(Vueltas)",
+        Clave: "VUGI",
+        Coordenadas: "22.518426916176647, -79.7032684525141"
+    },
+    {
+        Centro: "Vega Alta",
+        Clave: "VGA",
+        Coordenadas: "22.5503926307754, -79.81736905896837"
+    },
+    {
+        Centro: "Quinta",
+        Clave: "QT0",
+        Coordenadas: "22.466134207086228, -79.72971499029153"
+    },
+    {
+        Centro: "San Diego del Valle",
+        Clave: "DV0",
+        Coordenadas: "22.543871823602966, -80.09501010356999"
+    },
+    {
+        Centro: "Cifuentes",
+        Clave: "IF0",
+        Coordenadas: "22.649050240090798, -80.04854542850065"
+    },
+    {
+        Centro: "Mata",
+        Clave: "MAT",
+        Coordenadas: "22.626884327604813, -79.9328329250355"
+    },
+    {
+        Centro: "Wilfredo Pages",
+        Clave: "WPG",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Unidad Proletaria",
+        Clave: "UP0",
+        Coordenadas: "22.690259564906466, -80.01128214469028"
+    },
+    {
+        Centro: "Gavilanes",
+        Clave: "GB0",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Lugardita",
+        Clave: "LD0",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Corralillo",
+        Clave: "LL0",
+        Coordenadas: "22.985634009110093, -80.58553531990965"
+    },
+    {
+        Centro: "Motembo",
+        Clave: "OT0",
+        Coordenadas: "22.902781840429046, -80.68571827020122"
+    },
+    {
+        Centro: "Rancho Veloz",
+        Clave: "RV0",
+        Coordenadas: "22.879416096438813, -80.3898812951989"
+    },
+    {
+        Centro: "Rancho Veloz (WLL)",
+        Clave: "RV1",
+        Coordenadas: "22.879416096438813, -80.3898812951989"
+    },
+    {
+        Centro: "Sierra Morena",
+        Clave: "ZM0",
+        Coordenadas: "22.951937043975633, -80.51565951667686"
+    },
+    {
+        Centro: "Quintin Banderas",
+        Clave: "QUB",
+        Coordenadas: "22.854267111796133, -80.35250212026331"
+    },
+    {
+        Centro: "CAI Abel Santamaria",
+        Clave: "ASM",
+        Coordenadas: "22.61886908174378, -79.86346012942207"
+    },
+    {
+        Centro: "Calabazar de Sagua",
+        Clave: "CZ0",
+        Coordenadas: "22.648068451665456, -79.89906599310657"
+    },
+    {
+        Centro: "El Santo",
+        Clave: "EL0",
+        Coordenadas: "22.711704102271636, -79.68630616073241"
+    },
+    {
+        Centro: "Encrucijada",
+        Clave: "EN0",
+        Coordenadas: "22.61418440665221, -79.86818137936241"
+    },
+    {
+        Centro: "Emilio Cordova",
+        Clave: "ECV",
+        Coordenadas: "22.739534641600265, -79.75954068509968"
+    },
+    {
+        Centro: "Jutiero",
+        Clave: "JRO",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Manicaragua",
+        Clave: "MN0",
+        Coordenadas: "22.146505634945026, -79.97771352210657"
+    },
+    {
+        Centro: "La Campana",
+        Clave: "NK0",
+        Coordenadas: "22.15227764459337, -80.0667037599814"
+    },
+    {
+        Centro: "Seibabo",
+        Clave: "SI0",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Matagua",
+        Clave: "TA0",
+        Coordenadas: "22.237638284779862, -79.9983431244154"
+    },
+    {
+        Centro: "Guinia de Miranda",
+        Clave: "UI0",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Jibacoa",
+        Clave: "JBC",
+        Coordenadas: "22.010502824208334, -79.98097095606853"
+    },
+    {
+        Centro: "Yaya",
+        Clave: "YY0",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Moza",
+        Clave: "MZ0",
+        Coordenadas: "22.16729742903692, -80.02908975094202"
+    },
+    {
+        Centro: "Petriles",
+        Clave: "PL0",
+        Coordenadas: "22.01887018286813, -79.98840925267885"
+    },
+    {
+        Centro: "Herradura",
+        Clave: "HDA",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Baez",
+        Clave: "BZ0",
+        Coordenadas: "22.218909214480124, -79.75679474722148"
+    },
+    {
+        Centro: "Placetas",
+        Clave: "PT0",
+        Coordenadas: "22.317921007545202, -79.65680034592894"
+    },
+    {
+        Centro: "Falcon",
+        Clave: "FLC",
+        Coordenadas: "22.358772891832547, -79.76425410252065"
+    },
+    {
+        Centro: "Guaracabulla",
+        Clave: "GR0",
+        Coordenadas: "22.255634865349215, -79.73874873302503"
+    },
+    {
+        Centro: "Carbo Servia",
+        Clave: "CS0",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Hermanos Almejeiras",
+        Clave: "HA0",
+        Coordenadas: "22.314928465283632, -79.59011777473782"
+    },
+    {
+        Centro: "Quemado de Guines",
+        Clave: "QG0",
+        Coordenadas: "22.800514259893887, -80.25097237166236"
+    },
+    {
+        Centro: "Jose Ramon Riquelme",
+        Clave: "RL0",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Caguagua",
+        Clave: "CG0",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "CAI Carlos Caraballo",
+        Clave: "CCA",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Esperanza",
+        Clave: "EZ0",
+        Coordenadas: "22.4520232392235, -80.09618172164126"
+    },
+    {
+        Centro: "Jicotea",
+        Clave: "JT0",
+        Coordenadas: "22.518252382349154, -80.16130869642119"
+    },
+    {
+        Centro: "Ranchuelo",
+        Clave: "RH0",
+        Coordenadas: "22.371855711256696, -80.14983571011699"
+    },
+    {
+        Centro: "San Juan de los Yeras",
+        Clave: "YE0",
+        Coordenadas: "22.3343957865042, -80.10770892893059"
+    },
+    {
+        Centro: "Efrain Alfonso",
+        Clave: "EA0",
+        Coordenadas: "22.37756258610417, -80.19261543803121"
+    },
+    {
+        Centro: "Delicia",
+        Clave: "DL0",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Brillante",
+        Clave: "BR0",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Horqueta",
+        Clave: "HOT",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "General Carrillo",
+        Clave: "KR0",
+        Coordenadas: "22.263463007931357, -79.40593727348906"
+    },
+    {
+        Centro: "Remate de Ariosa",
+        Clave: "REA",
+        Coordenadas: "22.287352355187757, -79.48684815204408"
+    },
+    {
+        Centro: "Remedios",
+        Clave: "RM0",
+        Coordenadas: "22.50472712117027, -79.5462229636808"
+    },
+    {
+        Centro: "Zulueta",
+        Clave: "ZL0",
+        Coordenadas: "22.370059493060847, -79.56996283451295"
+    },
+    {
+        Centro: "CAI Heriberto Duquesne",
+        Clave: "CHD",
+        Coordenadas: "22.381122221236204, -79.49094092689944"
+    },
+    {
+        Centro: "Buena Vista",
+        Clave: "BV0",
+        Coordenadas: "22.356957760217654, -79.48307021786981"
+    },
+    {
+        Centro: "Viñas",
+        Clave: "VN0",
+        Coordenadas: "22.4310593753401, -79.52499603666138"
+    },
+    {
+        Centro: "Isabela de Sagua",
+        Clave: "IS0",
+        Coordenadas: "22.93850066216083, -80.00961053989806"
+    },
+    {
+        Centro: "Nueva Isabela",
+        Clave: "NISA",
+        Coordenadas: "22.85325245930998, -80.06967100036766"
+    },
+    {
+        Centro: "Sagua la Grande",
+        Clave: "SA0",
+        Coordenadas: "22.80391201496304, -80.07014768645006"
+    },
+    {
+        Centro: "GI 26-Julio (Sagua la Grande)",
+        Clave: "SAGI",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Sitiecito",
+        Clave: "SH0",
+        Coordenadas: "22.741274510596625, -80.0831471389261"
+    },
+    {
+        Centro: "La Rosita",
+        Clave: "ROSITA",
+        Coordenadas: "22.759399130664182, -80.00645916270595"
+    },
+    {
+        Centro: "Viana",
+        Clave: "VNA",
+        Coordenadas: "22.732033582161336, -79.99528352812555"
+    },
+    {
+        Centro: "Zona Salud",
+        Clave: "HV0",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Usuario HUAWEI",
+        Clave: "UMG",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Santa Clara",
+        Clave: "V00",
+        Coordenadas: "22.422851473144284, -79.95253543212579"
+    },
+    {
+        Centro: "ZH Contac Center",
+        Clave: "ZHCC",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "ZH Hospital Cardio Centro",
+        Clave: "ZHHC",
+        Coordenadas: "22.395651491935563, -79.96091100265023"
+    },
+    {
+        Centro: "ZH Hospital Infantil",
+        Clave: "ZHHI",
+        Coordenadas: "22.392298622317185, -79.95260688332996"
+    },
+    {
+        Centro: "ZH Hospital Materno",
+        Clave: "ZHHM",
+        Coordenadas: "22.390889996186733, -79.95329352877313"
+    },
+    {
+        Centro: "ZH Hospital Nuevo",
+        Clave: "ZHHN",
+        Coordenadas: "22.39240925023406, -79.95281339881527"
+    },
+    {
+        Centro: "ZH Hospital Viejo",
+        Clave: "ZHHV",
+        Coordenadas: "22.396941035632175, -79.96226283586641"
+    },
+    {
+        Centro: "GI Condado 1",
+        Clave: "CONDA1",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "GI Condado 2",
+        Clave: "CONDA2",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "GI Condado 3",
+        Clave: "CONDA3",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "La Panchita",
+        Clave: "PCH",
+        Coordenadas: "22.94065090144553, -80.40504736363452"
+    },
+    {
+        Centro: "Mariana Grajales",
+        Clave: "MRN0",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Jose Marti",
+        Clave: "JSM",
+        Coordenadas: "22.411786864360817, -80.0017751733068"
+    },
+    {
+        Centro: "Hatillo",
+        Clave: "HAT",
+        Coordenadas: "22.504707601701927, -80.00403384036177"
+    },
+    {
+        Centro: "Julian Grimau (Yabu)",
+        Clave: "JGM",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Gomera",
+        Clave: "GM0",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Anton Diaz",
+        Clave: "ADZ",
+        Coordenadas: "22.413842408964012, -80.03746093778399"
+    },
+    {
+        Centro: "Las Minas",
+        Clave: "MAS",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "Minerva",
+        Clave: "MIN0",
+        Coordenadas: "22.45392785318679, -79.80359034947327"
+    },
+    {
+        Centro: "Manajanabo",
+        Clave: "MNJ0",
+        Coordenadas: "22.384762394708126, -79.81726626598741"
+    },
+    {
+        Centro: "CAI George Washington",
+        Clave: "VW0",
+        Coordenadas: "22.5967719113104, -80.31460825394691"
+    },
+    {
+        Centro: "Santo Domingo",
+        Clave: "DG0",
+        Coordenadas: "22.579496153338447, -80.24989191816594"
+    },
+    {
+        Centro: "Santo Domingo",
+        Clave: "DGW",
+        Coordenadas: "22.579496153338447, -80.24989191816594"
+    },
+    {
+        Centro: "Cascajal",
+        Clave: "JS0",
+        Coordenadas: "22.670512280853224, -80.52844005837368"
+    },
+    {
+        Centro: "Manacas",
+        Clave: "MC0",
+        Coordenadas: "22.59803977147581, -80.33486429451975"
+    },
+    {
+        Centro: "Rodrigo",
+        Clave: "OD0",
+        Coordenadas: "22.67233796310718, -80.1971557721684"
+    },
+    {
+        Centro: "WLL (Lugardita y Amaro)",
+        Clave: "OH0",
+        Coordenadas: "22.681394905603693, -80.1428803823107"
+    },
+    {
+        Centro: "Topes de Collantes",
+        Clave: "TC0",
+        Coordenadas: "21.91427061090421, -80.01765853594472"
+    },
+    {
+        Centro: "Topes de Collantes",
+        Clave: "TCVC",
+        Coordenadas: "21.91427061090421, -80.01765853594472"
+    },
+    {
+        Centro: "WLL Cienfuegos",
+        Clave: "TCCF",
+        Coordenadas: "Longitud:"
+    },
+    {
+        Centro: "WLL Sancti Spíritus",
+        Clave: "TCSS",
+        Coordenadas: "Longitud:"
+    }
+];
