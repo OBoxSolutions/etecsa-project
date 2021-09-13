3a9ec2eacea9ece103b0e0a79383bf584a8289a7
Jose Garcia <jogarcia@tuta.io>
2021-09-13 16:47:29 -0400

    Remove makeMarkes call

--- resources/js/components/LeafletMap.vue
+++ resources/js/components/LeafletMap.vue
@@ -47,7 +47,6 @@ export default {
         });
 
         this.formatCoordinates();
-        this.makeMarkers();
 
         L.tileLayer(
             "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
