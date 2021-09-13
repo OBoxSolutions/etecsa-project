d98868d9315d8d35ce4a77cdb42f522aacb7563f
Jose Garcia <jogarcia@tuta.io>
2021-09-12 20:31:16 -0400

    Create isInDanger method

--- resources/js/components/BaseTable.vue
+++ resources/js/components/BaseTable.vue
@@ -23,6 +23,17 @@ export default {
             type: Array,
             default: () => []
         }
+    },
+    methods: {
+        isInDanger(indicator, ner) {
+            if (indicator === "ldne" && ner < 97.1) {
+                return true;
+            }
+            if (indicator === "ldie" && ner < 92) {
+                return true;
+            }
+            return false;
+        }
     }
 };
 </script>
